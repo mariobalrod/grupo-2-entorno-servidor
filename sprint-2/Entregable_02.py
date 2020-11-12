900dff1edbd53852f09b4eff10d45211beaab64b
mariobalrod <marioballestero.rdg@gmail.com>
2020-11-12 18:12:07 +0100

    estructura inicial para el Sprint 2

---
+++ sprint-2/Entregable_02.py
@@ -0,0 +1,23 @@
+# ==========================================================
+# Desarrollo Web - Entorno Servidor
+# Ciclo Superior Desarrollo Web
+# Curso 2020-21
+# Segunda entrega
+# ===========================================================
+
+# GRUPO:
+# INTEGRANTE 1:
+#   APELLIDOS, NOMBRE: Ballestero Rodriguez, Mario
+#   DNI: 29533046B
+# INTEGRANTE 2:
+#   APELLIDOS, NOMBRE: Del Junco Pérez, Esperanza
+#   DNI: 49137125V
+# INTEGRANTE 3:
+#   APELLIDOS, NOMBRE: Ávila Chacón, Sergio
+#   DNI: 29498790W
+
+# ===========================================================
+# Juego del UNO en Python
+
+def juego ():
+    print('Bienvenidos al Juego del UNO!')
