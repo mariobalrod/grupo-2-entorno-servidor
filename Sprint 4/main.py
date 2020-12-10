ed4312c5fdbf9516d6fbe1c6ff7ecedb7058f969
mariobalrod <marioballestero.rdg@gmail.com>
2020-12-10 20:34:36 +0100

    añadiendo cors al servidor

--- Sprint 4/main.py
+++ Sprint 4/main.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
+# Iniciar app flask
 app = Flask(__name__)
+
+# Configuracion necesaria para la app flask
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+app.config['DEBUG'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Añadiendo Cors a la app
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Añadiendo socketio a la app y cors a los socket
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
-    return 'hola soy el servidor'
+    return 'Hola soy el servidor'
 
+# Método principal main (Inicializando servidor)
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
