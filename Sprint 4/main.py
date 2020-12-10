ce28fe1aedd77b837164bf5ed059dd159657b644
sergioavilachacon <sergioavilachacon@gmail.com>
2020-12-10 20:10:09 +0100

    añadiendo socketIO al servidor flask

--- Sprint 4/main.py
+++ Sprint 4/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return 'hola soy el servidor'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
