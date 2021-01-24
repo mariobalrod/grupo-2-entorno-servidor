622450c2ca959de418ab1991cd6338beec17c540
espe <esperanza.del.junco@adaits.es>
2021-01-24 18:02:36 +0100

    register conectado

--- Sprint 6/mediam/auth/views.py
+++ Sprint 6/mediam/auth/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'auth/login.html')
+
+def register(request):
+    return render(request, 'auth/register.html')
