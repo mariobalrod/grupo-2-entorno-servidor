b801d52575bbdf1508e23884043761924afe0cb0
espe <esperanza.del.junco@adaits.es>
2021-01-24 18:05:25 +0100

    reset_password conectado

--- Sprint 6/mediam/auth/views.py
+++ Sprint 6/mediam/auth/views.py
@@ -6,3 +6,6 @@ def login(request):
 
 def register(request):
     return render(request, 'auth/register.html')
+
+def reset_password(request):
+    return render(request, 'auth/reset_password.html')
