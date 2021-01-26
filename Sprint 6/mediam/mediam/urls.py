d3282536e1ec18b85e68474ba6e2368e3d1b5d29
Mario <marioballestero.rdg@gmail.com>
2021-01-26 23:15:59 +0100

    final cabecera!

--- Sprint 6/mediam/mediam/urls.py
+++ Sprint 6/mediam/mediam/urls.py
@@ -2,15 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('auth/', include('auth.urls')),
-    path('perfil/', include('perfil.urls')),
-    path('cabecera/', include('cabecera.urls')),
-=======
     path('', include('home.urls')),
     path('', include('auth.urls')),
     path('', include('perfil.urls')),
     path('', include('chat.urls')),
     path('', include('upload_image.urls')),
->>>>>>> 6a4eac1e576acbd4060f31f7ccfcb012a4cfa4ad
 ]
