6418ecb3c43deca630793abd4bc3a9c6912a757f
mariobalrod <marioballestero.rdg@gmail.com>
2021-01-27 20:21:38 +0100

    add chat

--- Sprint 6/mediam/chat/views.py
+++ Sprint 6/mediam/chat/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def chat(request):
-    return render(request, 'chat/chat.html')
+    return render(request, 'chat/chat.html', {'range': [1, 2, 3, 4, 5, 6, 7, 8]})
