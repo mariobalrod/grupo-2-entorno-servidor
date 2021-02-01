51e6f05c884958d83eb329dc70e9cc3f127a0ba2
espe <esperanza.del.junco@adaits.es>
2021-02-01 18:10:16 +0100

    model chat y message

--- Sprint 7/mediam/chat/models.py
+++ Sprint 7/mediam/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.http import request
+from user.models import User
 
 # Create your models here.
+class Chat(models.Model):
+    user_1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_2 = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_user = models.CharField(max_length=264, unique=True)
+
+class Message(models.Model):
+    message = models.CharField(max_length=264, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_date = models.DateField()
+
